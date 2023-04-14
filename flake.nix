c8b572ef7f66cfe2d05bf5f653547fe40d3f2655
Brian Ketelsen <bketelsen@gmail.com>
2023-04-14 14:25:28 -0400

    test

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  inputs = {
+    # Principle inputs (updated by `nix run .#update`)
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixos-flake.url = "github:bketelsen/nixos-flake";
+  };
+
+  outputs = inputs@{ self, ... }:
+    let
+      fleekConfig = builtins.fromJSON (builtins.readFile ./.fleek.json);
+    in
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      imports = [
+        inputs.nixos-flake.flakeModule
+        ./fleek.nix
+      ];
+
+      flake.homeModules.default = ./home.nix;
+      flake.fleekConfig = fleekConfig;
+
+      flake.templates.default = {
+        description = "A `home-manager` template providing useful tools & settings for Nix-based development";
+        path = builtins.path {
+          path = ./.;
+          filter = path: _:
+            inputs.nixpkgs.lib.hasSuffix ".nix" path ||
+            inputs.nixpkgs.lib.hasSuffix ".lock" path;
+        };
+      };
+
+      perSystem = { self', pkgs, ... }:
+        {
+          legacyPackages.homeConfigurations.beast =
+            self.nixos-flake.lib.mkHomeConfiguration
+              pkgs
+              ({ pkgs, ... }: {
+                imports = [ self.homeModules.default ];
+                home.username = fleekConfig.username;
+                home.homeDirectory = "/${if pkgs.stdenv.isDarwin then "Users" else "home"}/${fleekConfig.username}";
+                home.stateVersion = "22.11";
+              });
+
+          # Enables 'nix run' to activate.
+          apps.default.program = self'.packages.activate-home;
+
+          # Enable 'nix build' to build the home configuration, but without
+          # activating.
+          packages.default = self'.legacyPackages.homeConfigurations.${fleekConfig.username}.activationPackage;
+        };
+    };
+}
